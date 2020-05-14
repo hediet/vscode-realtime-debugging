65e38dd051021604c034dd1aa31ed70ce0d5233d
Henning Dieterichs <h.dieterichs@knuddels.de>
2020-05-15 00:59:21 +0200

    Adds typescript demo.

--- demo/chrome/src/index.js
+++ demo/chrome/src/index.js
@@ -2,16 +2,9 @@ window.onmousemove = (data) => {
 	console.log(`mouse pos: ${data.x} ${data.y}`);
 
 	if (data.x > 400) {
-
-		myFunction(); 
-
+		myFunction();
 	} else {
 		console.log("x <= 400");
 	}
-};
-
-
-
-
 
 function myFunction() {}
